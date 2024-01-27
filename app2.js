187e6f39fd7a8c91207bbe6c68c2c041a12cb21c
aleOraOne <157917078+aleOraOne@users.noreply.github.com>
2024-01-27 18:27:11 -0300

    Version Final Juego Numero Secreto

---
+++ app2.js
@@ -0,0 +1,20 @@
+let numerosSorteados = [];
+console.log(numerosSorteados);
+
+numerosSorteados.push(5);
+console.log(numerosSorteados);
+
+numerosSorteados.push(22);
+console.log(numerosSorteados);
+
+console.log(numerosSorteados.length);
+console.log(numerosSorteados[0]);
+
+numerosSorteados.push(27);
+console.log(numerosSorteados);
+
+numerosSorteados.push(55);
+console.log(numerosSorteados);
+
+console.log(numerosSorteados[1]);
+console.log(numerosSorteados[numerosSorteados.length-1]);
